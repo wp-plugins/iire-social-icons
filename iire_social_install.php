b4831c3d14d85383b6dcc87a13a83e79d5c12e17
srussell13 <srussell13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-18 00:14:37 +0000

    Version 0.41 Backup
    git-svn-id: https://plugins.svn.wordpress.org/iire-social-icons/tags/0.41@640862 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iire_social_install.php
@@ -0,0 +1,168 @@
+<?php
+// Social Icons Installation - 12-13-2012
+
+global $iire_social_version;
+$iire_social_version = "0.41";
+
+// INSTALL/CREATE TABLES
+function iire_social_install() {
+	global $wpdb;
+	global $iire_social_version;
+
+	$table_name = $wpdb->get_blog_prefix($blog_id)."iire_social";
+
+	$sql = "CREATE TABLE IF NOT EXISTS ".$table_name." (
+	option_id INT(11) NOT NULL AUTO_INCREMENT,
+	option_name VARCHAR(255) NOT NULL,
+	option_value VARCHAR(4096) NOT NULL,
+	PRIMARY KEY  (option_id)	
+	);";
+	
+	require_once(ABSPATH . 'wp-admin/upgrade-functions.php');		
+	dbDelta($sql);	
+	
+	$wpdb->print_error();		
+
+	add_option("iire_social_version", $iire_social_version);
+	add_option("iire_social_data", '0');
+
+   //Check for update
+   $installed_ver = get_option( "iire_social_version" );
+   if( $installed_ver != $iire_social_version ) {
+		$sql = "CREATE TABLE IF NOT EXISTS ".$table_name." (
+		option_id INT(11) NOT NULL AUTO_INCREMENT,
+		option_name VARCHAR(255) NOT NULL,
+		option_value VARCHAR(4096) NOT NULL,/
+		PRIMARY KEY  (option_id)
+		);";
+
+		require_once(ABSPATH . 'wp-admin/upgrade-functions.php');		
+		dbDelta($sql);
+		update_option( "iire_social_version", $iire_social_version ); // Updates WP Options table
+	}   
+}
+
+function iire_update_social_check() {
+    global $wpdb;
+	global $iire_social_version;
+	global $current_blog;
+		
+    if (get_site_option('iire_social_version') != $iire_social_version) {
+        iire_social_install();
+    }
+	
+	$table = $wpdb->prefix."iire_social";
+
+    if (get_site_option('iire_social_data') == '0') {
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('1', 'registration_email', '')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('2', 'registration_key', '')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('3', 'registration_activated', '')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('4', 'registration_expired', '')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('5', 'registration_version', 'FE01CE2A')");	
+	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('6', 'email_recipient', 'you@yoursite.com')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('7', 'email_sender', 'webmaster@yoursite.com')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('8', 'email_cc', '')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('9', 'email_bcc', '')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('10', 'email_subject', 'Contact Information')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('11', 'email_message', 'Add your message here...')");			
+
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('12', 'clone_widget_settings', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('13', 'addthis', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('14', 'addthis_key', '')");
+
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('15', 'jquery', '')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('16', 'jquery_ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js')");			
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('17', 'jquery_ui_css', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/jquery-ui.css')");								
+	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('18', 'link_target', '_blank')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('19', 'link_title', '1')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('20', 'link_nofollow', '1')");
+	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('21', 'css_cache', '0')");		
+
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('23', 'themes', 'default;iphone;chrome_panels;circle_cutouts;eco_green;gold_bars;light_bulbs;post_it_notes;punch_thru;red_alert;stickers;symbols_black;symbols_gray;symbols_white;wood_crates;custom1;custom2;custom3;custom4;custom5')");
+	
+		// WIDGET	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('24', 'widget_icon_theme', 'default')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('25', 'widget_icon_size', '64')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('26', 'widget_icon_spacing', '10')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('27', 'widget_icon_opacity', '100')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('28', 'widget_icon_bgcolor', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('29', 'widget_icon_bgcolor_up', 'AAFF00')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('30', 'widget_icon_bgcolor_hover', '00AAFF')");			
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('31', 'widget_dropshadow', '1')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('32', 'widget_dropshadow_color', 'AAAAAA')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('33', 'widget_dropshadow_horizontal_offset', '3')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('34', 'widget_dropshadow_vertical_offset', '3')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('35', 'widget_dropshadow_blur', '8')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('36', 'widget_roundedcorners', '1')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('37', 'widget_roundedcorners_topleft', '4')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('38', 'widget_roundedcorners_topright', '4')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('39', 'widget_roundedcorners_bottomleft', '4')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('40', 'widget_roundedcorners_bottomright', '4')");					
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('41', 'widget_orientation', 'horizontal')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('42', 'widget_align', 'left')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('43', 'widget_width', '225')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('44', 'widget_height', '225')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('45', 'widget_pad_top', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('46', 'widget_pad_left', '0')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('47', 'widget_pad_bottom', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('48', 'widget_pad_right', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('49', 'widget_margin_top', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('50', 'widget_margin_left', '0')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('51', 'widget_margin_bottom', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('52', 'widget_margin_right', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('53', 'widget_background', '0')");					
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('54', 'widget_bg_color', 'FFFFFF')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('55', 'widget_border_color', 'DDDDDD')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('56', 'widget_border_size', '0')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('57', 'widget_css', '')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('58', 'widget_addclasses', 'default horizontal sp10 dropshadow roundedcorners opacity icon64')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('59', 'widget_icons', '')");				
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('60', 'widget_output', '')");
+		
+		// SHORTCODE
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('63', 'sc_icon_theme', 'default')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('64', 'sc_icon_size', '32')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('65', 'sc_icon_spacing', '10')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('66', 'sc_icon_opacity', '100')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('67', 'sc_icon_bgcolor', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('68', 'sc_icon_bgcolor_up', 'AAFF00')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('69', 'sc_icon_bgcolor_hover', '00AAFF')");			
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('70', 'sc_dropshadow', '1')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('71', 'sc_dropshadow_color', 'AAAAAA')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('72', 'sc_dropshadow_horizontal_offset', '3')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('73', 'sc_dropshadow_vertical_offset', '3')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('74', 'sc_dropshadow_blur', '8')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('75', 'sc_roundedcorners', '1')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('76', 'sc_roundedcorners_topleft', '4')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('77', 'sc_roundedcorners_topright', '4')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('78', 'sc_roundedcorners_bottomleft', '4')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('79', 'sc_roundedcorners_bottomright', '4')");					
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('80', 'sc_orientation', 'horizontal')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('81', 'sc_align', 'left')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('82', 'sc_width', '600')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('83', 'sc_height', '70')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('84', 'sc_pad_top', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('85', 'sc_pad_left', '0')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('86', 'sc_pad_bottom', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('87', 'sc_pad_right', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('88', 'sc_margin_top', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('89', 'sc_margin_left', '0')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('90', 'sc_margin_bottom', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('91', 'sc_margin_right', '0')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('92', 'sc_background', '0')");					
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('93', 'sc_bg_color', 'FFFFFF')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('94', 'sc_border_color', 'DDDDDD')");		
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('95', 'sc_border_size', '0')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('96', 'sc_css', '')");
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('97', 'sc_addclasses', 'default horizontal sp10 dropshadow roundedcorners opacity icon32')");	
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('98', 'sc_icons', '')");				
+		$wpdb->query("INSERT INTO $table (option_id, option_name, option_value) VALUES ('99', 'sc_output', '')");
+		
+		update_option("iire_social_data", '1');		
+	}			
+}
+add_action('plugins_loaded', 'iire_update_social_check');
+?>
---
+++ readme.txt
@@ -0,0 +1,201 @@
+=== Plugin Name ===
+Contributors: iiRe Productions
+Donate link: http://iireproductions.com/web/website-development/wordpress-plugins/plugins-social-icons/
+Tags: Social Media, Icons, Facebook, Google, Instagram, Linked In, Pinterest, Skype, Twitter, YouTube  
+Requires at least: 3.1
+Tested up to: 3.5
+Stable tag: 0.41 
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add social media icons and links to your site with a customizable user interface. Majority of social networks are supported!
+
+== Description ==
+
+Easily choose an from a library of social media icons, customize their appearance, and add them to a wordpress website as a widget or shortcode. Use one the default icon themes or use the symbols and background colors with CSS styling.
+
+See our <a href="http://iireproductions.com/web/website-development/wordpress-plugins/plugins-social-icons/">iiRe Social Icons page</a> for more information.
+
+
+**Features**
+
+* Create an independent social media icons widget and shortcode
+* Majority of social networks supported
+* Over 80 different icons!
+* 5 different icon sizes (16x16px, 24x24px, 32x32px, 48x48px, 64x64px)!
+* Additional free icon themes available
+* Real-time editing! Change icon theme, size. spacing, etc. dynamically
+* Drag and drop icon reordering!
+* Drag and drop delete!
+* Add custom titles to each icon
+* Add custom links to each icon (samples provided)
+* Add CSS rounded corners! Customize the radius of each corner
+* Add CSS drop shadows! Customize the color, hortizonal and vertical offset and blur
+* Add background colors!
+* Add background hover colors!
+* Add CSS opacity!
+* Links in new or same window/tab (link target)
+* Show or Hide the title (alt/title tag)
+* Hide links from search engines (no follow)
+* Stack icons horizontally or vertically!
+* Align icons left or right
+* Customize the width and height of the icons container
+* Customize the CSS padding and margins of the icon container 
+* Customize the container border size and color
+* Customize the container background color
+* Add This social media support
+* Built-in email contact form w/ recipient settings
+* Built-in print function
+* A "Reset" button!
+* Works in most modern browsers, including IE9!
+* Accordian-style options panel
+* Options panel is collapsible!
+* Add up to five custom themes
+
+
+**Supported Networks**
+
+500px, Add This, Amazon, Aim, AOL, Apple, Android, Beboo, Badoo, Blink List, Blogger, Buzznet, Cafe Mom, Delicious, Design Float, Deviant Art, Digg, Dribble, Ebay, Esty, Evernote, Feedburner, Facebook, Flickr, Friendfeed, Friendster, Foursquare, Geocaching, Google, Google Plus, Gmail, Hi5, Instagram, Last FM, Linked In, Live Journal, Meetup, Microsoft, Mixx, Myspace, Newsvine, Ning, Orkut, Paypal, Pinterest, Picasa, Pure VOlume, Reddit, Reverbnation, RSS, Share This, Skype, Slash Dot, Slideshare, Smug Mug, Sound Cloud, Spotify, Stumbleupon, Tagged, Twitter, Vimeo, Wordpress, Yahoo, Yelp, You Tube
+
+There are additional icons for email, custom links, alternate website, add to favorites, more information, chat, print and a contact us page.
+
+
+**Quick Start**
+
+1. Go to 'Wordpress Admin', 'Appearance', 'Widgets'.
+2. Add the 'iiRe Social Media Icons' to a sidebar widget area and 'Save'.
+3. Go to 'Wordpress Admin', 'iiRe Social Icons', 'Widget Settings'.
+4. Click 'Icon Theme' in the side panel, choose a theme in 'Icon Theme'or use the default.
+5. In the Icons section, click an icon to add it to the Widget Designer.
+6. Repeat the previous step to add additional icons.
+7. Double-click each icon in the Widget Designer to edit the link and title.
+8. Click 'Save Changes'.
+9. View the section of your website/blog wher you place the widget to view the output!
+
+
+== Installation ==
+
+For an automatic installation through WordPress:
+
+1. Go to the 'Add New' plugins screen in your WordPress admin area.
+2. Search for 'iiRe Social.'
+3. Click 'Install Now' button.
+4. Activate the plugin to finish the installation.
+
+For a manual installation via FTP:
+
+1. Upload the iire-social-icons folder to the /wp-content/plugins/ directory.
+2. Activate the plugin through the 'Plugins' screen in your WordPress admin area.
+
+To upload the plugin through WordPress, instead of FTP:
+
+1. Upload the downloaded zip file on the 'Add New' plugins screen in your WordPress admin area.
+2. Activate the plugin through the 'Plugins' screen in your WordPress admin area.
+
+
+To install additional icon themes via FTP:
+
+1. Download the desired icon theme to your hardrive and unzip. (The name of the zip file is the name of the theme folder)
+2. Upload the icon theme folder to the /wp-content/plugins/iire-social-icons/themes/ directory.
+
+
+
+== Frequently Asked Questions ==
+
+**Q. What are the plugin requirements?** 
+A. The admin section requires Jquery and Jquery UI to be enabled. These libraries are usually included with your default Wordpress installation, then initialized on the widget and shortcode admin pages.
+
+**Q. Why two different admin panels for the widget and the shortcode?** 
+A. All settings for the widget and the shortcode are stored independently. The widget(best used in the sidebar) and the shortcode(best used on a page or post) can be designed differently, with different themes, size, icon order, etc.
+
+**Q. Can I add my own icons?** 
+A. Yes, you can create a "custom1" (up to 5 respectively) folder under the plugin themes directory. Each icon theme is designed as a set of individual image sprites (All the icons are arranged in a grid as a single image), so your "icon grid" must match the layout on one of the other themes. Icons are not available as individual files.
+
+**Q. How were the icons created?** 
+A. All icons were designed in-house at iiRe Productions using high-resolution vector images. This method helps us to produce the highest quality images.
+
+**Q. How do I access all the icons?** 
+A. The free version of this plugin has limited access to certain social networks. If you would like to receive a registration code to unlock these, please consider making a donation.
+
+**Q. Are there additional icon themes?** 
+A. Yes, there are additional free theme available as downloadable zip file on http://iireproductions.com/web/website-development/wordpress-plugins/plugins-social-icons/
+
+**Q. How do I change the default links for each icon?** 
+A. In the Widget or Shortcode Designer, just double-click the desired icon to display the title/link icon and enter the appropriate values.   
+
+
+== Screenshots ==
+
+1. Widget Designer Admin Panel
+2. Shortcode Designer Admin Panel
+
+
+== Changelog ==
+
+= 0.41 Make sure to deactivate and delete all prior versions! =
+* Updated the footer initialization to  only use jQuery UI if the email function is active and a proper recipient address is specified
+* Updated the FAQ
+* Updated the plugin landing page
+* Updated the initialization of jQuery UI library files in the Admin section
+* Fixed an PHP 5.0 date error in the send email function
+* Updated readme.txt file
+
+
+= 0.40 =
+* Updated the Google CDN links for jQuery UI for Wordpress 3.5 compatibility!!!
+* Updated the included jQuery UI library files
+* Fixed the email dialog in Wordpress 3.5
+* Fixed an issue with an MYSQL insert query on installation
+* Fixed an issue with the dynamic CSS generator
+* Updated the installation function
+* Undated the uninstall function
+* Removed the admin default email address from the Email section
+* Added support for additional free iiRe themes
+* Updated readme.txt file
+
+
+= 0.30 =
+* Added the following networks: Esty, Buzznet, Paypal, Reverbnation, Ebay, Mixx, Pure Volume, Design Float, 500px, Smugmug, GoeCaching, Evernote
+* Added support for up to 5 custom folders using the grid layout
+* Added a Print icon and function
+* Changed the default icon size on reset to 64x64 for the widget
+* Changed the default icon size on reset to 32x32 for the shortcode
+* Added additional detection of jQuery UI
+* Included the jQuery UI base javascript, CSS and image files
+* Fixed issue when using plugin on a site that uses a SSL certificate.
+* Updated the Widget/Shortcode Designer screenshots
+* Updated readme.txt file
+
+
+= 0.21 =
+* Fix an issue where themes were not properly detected when plugin was installed into a different location other that the default folder
+* Rewrote the javascript for the "Add to Favorites" Works in FireFox and IE, a prompt is displayed for Chrome,Safari and Opera  
+* Adjusted the default minimum height of the Widget/Shortcode Designer preview area
+* Added javascript to hide Wordpress nag messages when editing in the Widget/Shortcode Designer
+* Added a "blank" icon - Designers can graphically modify a theme to add an icon logo that may not be available in the network.
+* Updated the uninstaller to properly delete the database table upon deactivation.  
+* Additional support for future upgrades.
+* Updated readme.txt file
+
+= 0.20 =
+* Fixed an issue with icon opacity in the Widget/ShortCode Designers
+* Fixed an issue remembering the active theme in Shortcode Designer
+* Added a prefix to each icon ID to allow additional compatiblilty with 3rd party themes that also include social media icons 
+* Modified all stylesheets to use the new icon prefix
+* Updated readme.txt file
+
+= 0.12 =
+* Number of available themes included was reduced to limit the size of the plugin
+* Additional theme files posted on website
+* Select box for theme code modified to detect new sub-folders in the plugin theme directory
+* Updated readme.txt file
+
+= 0.11 =
+* Limited number of meta tags in readme.txt file
+
+= 0.10 =
+* Initial beta release
+== Upgrade Notice ==
+** Make sure to deactivate and delete all prior versions! **
+
+See our <a href="http://iireproductions.com/web/website-development/wordpress-plugins/plugins-social-icons/">iiRe Social Icons page</a> for more information.
